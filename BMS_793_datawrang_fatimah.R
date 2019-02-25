50097a965c4640ebe66eca0b9518e1a0e47908af
Darren Gemoets <dgemoets@gmail.com>
2019-02-25 14:57:48 -0500

    Edits.

--- BMS_793_datawrang_fatimah.R
+++ BMS_793_datawrang_fatimah.R
@@ -9,14 +9,14 @@ ggplot(fatim.data, aes(x=time, y=amp, color=eye))+geom_point()
 fatim.data %>% group_by(mouse.id,eye) %>% summarise(mean=mean(amp))
 
 ## mean between L/R eye for each time point
-fatim.data %>% group_by(mouse.id,time) ## %>% summarise(mean=mean(amp))
+fatim.data %>% group_by(mouse.id,time) %>% summarise(mean=mean(amp))
 
 ## subset of time >= 0
 fatim.data.postme <- subset(fatim.data, time > -1)
 
 ## shift amp by amp at t=0
 fatim.data.postme$shft.amp <- NA
-fatim.data.postme %>% group_by(id) %>% mutate(shft.amp = amp-amp[time==0])
+fatim.data.postme %>% group_by(id) %>% mutate(shft.amp = amp-amp[time==0]) -> tempp
 
 ## find max and min (do we need eye?)
 fatim.data %>% group_by(id) %>% summarise(max=max(amp))
@@ -27,20 +27,19 @@ fatim.data %>% group_by(id) %>% summarise(min=min(amp))
 dg.data <- read.csv("/Volumes/New Volume/flash_2.8.2019/BMS_793B/data/BMS_793_dg_abund_data.csv")
 
 ## separate tax column into two
-dg.data$tax1 <- dg.data$tax2 <- NA
+dg.data$tax2 <- dg.data$tax1 <- NA
 temp <- strsplit(as.character(dg.data$tax),"[;]")
 
 for (i in 1:5){
   dg.data$tax1[i] <- temp[[i]][1]
   dg.data$tax2[i] <- temp[[i]][2]
-  
 }
 
 temp <- colSums(dg.data[,-c(1,6,7)])  ## 1,6,7 have the taxa  
-dg.data.prop <- dg.data
+dg.data.prop <- dg.data  # defining a data frame
 dg.data.prop[,-c(1,6,7)] <- sweep(dg.data[,-c(1,6,7)],MARGIN=2,temp,"/")
 
-dg.data.tax1 <- aggregate(.~tax1,dg.data.prop[,-c(1,7)],sum)
-dg.data.tax2 <- aggregate(.~tax2,dg.data.prop[,-c(1,6)],sum)
+dg.data.tax1 <- aggregate(.~tax1,dg.data.prop[,-c(1,6)],sum)
+dg.data.tax2 <- aggregate(.~tax2,dg.data.prop[,-c(1,7)],sum)
 
   
